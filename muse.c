b99779444b4217ab5d55491279e33585e3e273c1
Karel Tuma <karel.tuma@gmail.com>
2012-09-19 02:15:29 +0200

    Update contact info
    Signed-off-by: Karel Tuma <karel.tuma@gmail.com>

--- muse.c
+++ muse.c
@@ -1,7 +1,7 @@
 /*
  * MUSE filesystem
  *
- * Copyright (c) 2005-2006 Karel Tuma, leet.cz.
+ * Copyright (c) 2005-2012 Karel Tuma, karel.tuma@gmail.com.
  * 
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
